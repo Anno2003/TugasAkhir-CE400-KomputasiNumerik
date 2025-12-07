9cf9ec652bf6400513a9bc7903dcedc526230542
anno2003 <idsale2002@yahoo.com>
2025-12-07 16:55:31 +0700

    added lagrange polynomial interpolation function

--- src/interpolasi.py
+++ src/interpolasi.py
@@ -1,2 +1,17 @@
 # TODO: lagrange interpolation
 #
+
+def lagrange_polynomial(x_values, y_values):
+    n = len(x_values)
+
+    def P(x):
+        total = 0
+        for i in range(n):
+            term = y_values[i]
+            for j in range(n):
+                if i != j:
+                    term *= (x - x_values[j]) / (x_values[i] - x_values[j])
+            total += term
+        return total
+
+    return P
